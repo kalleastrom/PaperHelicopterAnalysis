dc336a789c2bdb664ba4069f8777d864438b1a65
jastermark <j_astermark@hotmail.com>
2017-02-01 16:53:33 +0100

    Added Jonathans files for reading video input

--- matlab/angleCalc.m
+++ matlab/angleCalc.m
@@ -1,20 +1,21 @@
-function a = angleCalc(im)
+function [a,c,direction] = angleCalc(im)
 %Takes a grayscale image and returns the angle of the eigen vector
 %corresponding to the largest eigen value to the second moment matrix.
 %im = imread('..\data\testBild.jpg');
-im = rgb2gray(im);
+% im = rgb2gray(im);
 %cropIm = im(100:500,1:405);
 %bIm = cropIm>100;
 %imagesc(bIm)
 %colormap(gray)
 %hold on
-img = im2double(bIm);
+img = im2double(im);
 [H,c] = secondMoment(img);
-[V E] = eig(H)
-[vals ind] = max(max(E))
-ev = V(:,ind)
-angle = atan(ev(2)/ev(1))
-a=normangle2(angle)
+[V E] = eig(H);
+[vals ind] = max(max(E));
+ev = V(:,ind);
+angle = atan(ev(2)/ev(1));
+a=normangle2(angle);
+direction = ev;
 %grads = angle *180/pi
 
 % p1 = c
